4054b294d91c4101f36cfc0c52d960895feba789
Hudson <hava12@naver.com>
2022-07-02 16:54:12 +0900

    Initial commit

---
+++ src/main/java/com/bhd/mdmh/MdmhApplication.java
@@ -0,0 +1,13 @@
+package com.bhd.mdmh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmhApplication.class, args);
+	}
+
+}
